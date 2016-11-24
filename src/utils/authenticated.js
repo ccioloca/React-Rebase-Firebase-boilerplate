eced483ebc816c61407934ac4d3817c4b85121df
qwales1 <qwales1@gmail.com>
2016-11-24 01:00:28 -0800

    update requireAuth to use base.onAuth

--- src/utils/authenticated.js
+++ src/utils/authenticated.js
@@ -1,12 +1,14 @@
 import base from '../rebase.config.js'
 
-export const requireAuth = (nextState, replace) => {
-
-    if(null === base.auth().currentUser || false === base.auth().currentUser.emailVerified ) {
-        replace({
-            pathname: '/login',
-            state: { nextPathname: nextState.location.pathname }
-        })
-    }
-
+export const requireAuth = (nextState, replace, cb) => {
+	const unsubscribeToListener = base.onAuth(user => {
+		if (!user) {
+			replace({
+				pathname: '/login',
+				state: { nextPathname: nextState.location.pathname }
+			});
+		}
+		unsubscribeToListener();
+		cb();
+	});
 }
