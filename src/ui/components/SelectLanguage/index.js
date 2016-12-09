9f865f83be663092da0cfcc5b2fdada4fe93ae2f
paulhayes <water7earth@gmail.com>
2016-12-09 20:10:26 +0100

    improves SelectLanguage component code

--- src/ui/components/SelectLanguage/index.js
+++ src/ui/components/SelectLanguage/index.js
@@ -1,32 +1,26 @@
 import React from 'react'
+import Card from '../../layout/Card'
 
 const SelectLanguage = ({ handleOptionChange, language, Text, availableLanguages }) => {
-
-  const mappedLanguages = availableLanguages.map((item, index) => {
-
-    const checked = language === item
-    return (
-      <div key={index} className="select-language__radio-wrapper">
-        <label className="select-language__label">
-
-          <input  className="select-language__input"
-                  type="radio"
-                  name="language"
-                  value={item}
-                  checked={checked}
-                  onChange={ () => handleOptionChange(item) } />
-          { Text[item].language }
-
-        </label>
-      </div>
-    )
-  })
-
   return (
-    <form className="select-language">
-      <h1>{Text[language].selectYourLanguage}</h1>
-      {mappedLanguages}
-    </form>
+    <Card>
+      <form className="select-language">
+        <h1>{Text[language].selectYourLanguage}</h1>
+        { availableLanguages.map((item, index) =>
+          <div key={index} className="select-language__radio-wrapper">
+            <label className="select-language__label">
+              <input  className="select-language__input"
+                      type="radio"
+                      name="language"
+                      value={item}
+                      checked={language === item}
+                      onChange={ () => handleOptionChange(item) } />
+              { Text[item].language }
+            </label>
+          </div>
+        )}
+      </form>
+    </Card>
   )
 }
 
