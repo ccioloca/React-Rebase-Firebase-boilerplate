257aa73dd3131f2b425d25f5991f189fc6c43f99
paulhayes <water7earth@gmail.com>
2017-01-02 09:26:31 +0100

    wip styles

--- src/ui/components/NewNote/index.js
+++ src/ui/components/NewNote/index.js
@@ -2,34 +2,37 @@ import React from 'react'
 
 const NewNote = ({onFormSubmit, onChange, onCheck, isChecked, displayName, photoURL, value, Text, language }) => {
 
+  const MODULE_NAME = 'new-note-form'
   let note
 
   return (
-    <form className="form new-note-form"
+    <form className={`form ${MODULE_NAME}`}
           onSubmit={(event) => onFormSubmit(event, {
             note: note.value,
             photoURL,
             displayName,
             date: Date.now()
     })}>
-      <div className="form__row new-note-form__row">
-        <label className="form__label new-note-form__label">
-          {Text[language].note}:
+      <div className={`form__row ${MODULE_NAME}__row`}>
+        <label className={`form__label ${MODULE_NAME}__label ${MODULE_NAME}__title`}>
+          {Text[language].addNewNote}
         </label>
-        <textarea className="form__field form__textarea new-note-form__field new-note-form__textarea"
+        <textarea className={`form__field form__textarea ${MODULE_NAME}__field ${MODULE_NAME}__textarea`}
                ref={c => (note = c)}
                value={value}
                onChange={() => onChange(note.value) }
                />
+       <input className={`form__submit ${MODULE_NAME}__submit`} type="submit" value={Text[language].addNewNote} />
       </div>
-      <input className="form__submit new-note-form__submit" type="submit" value={Text[language].submit} />
+
       <label>
-        <input  className="form__checkbox new-note-form__checkbox"
+        <input  className={`form__checkbox ${MODULE_NAME}__checkbox`}
                 type="checkbox"
                 checked={isChecked}
                 onChange={ () => onCheck() } />
         {Text[language].isPrivate}
       </label>
+
     </form>
   )
 
